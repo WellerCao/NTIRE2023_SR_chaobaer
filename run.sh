f3c05e1fa23c2d7b04e65c392592f9c126b03f57
WellerCao <37168337+WellerCao@users.noreply.github.com>
2023-03-22 21:25:58 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=0 python test_demo.py --data_dir /home/mi/4T/cj/StereoSR/DVI2K_Dataset/NTIRE2023SR_submit_test --save_dir /home/mi/4T/cj/StereoSR/DVI2K_Dataset/NTIRE2023SR_submit_test/results --model_id 14
+
